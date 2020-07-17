71d52a0a73985c1439f8aacf6fa5669fc0839549
FrankBlack <fonyeabo12@gmail.com>
2020-07-17 10:19:43 -0500

    db created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chilli', false);
